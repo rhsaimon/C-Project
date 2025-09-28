364e6a9ca9706b6ac9a4fa89c71a7d75fe8184a8
rhsaimon <robiulhasan007007@gmail.com>
2025-09-28 14:46:29 +0600

    First practice commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace gentle
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string name = "sam";
+			string name2 = "bhai";
+			Console.WriteLine("Hello, " + name + " " + name2);
+		}
+	}
+}
